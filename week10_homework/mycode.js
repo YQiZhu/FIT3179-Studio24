7fa188a14de2033b27ce0a01d59321b7bad5b000
YQiZhu <111552343+YQiZhu@users.noreply.github.com>
2022-10-10 00:42:21 +1100

    Add files via upload

---
+++ week10_homework/mycode.js
@@ -0,0 +1,6 @@
+var vg_1 = "gdp.vg.json";
+vegaEmbed("#bar1", vg_1).then(function(result) {
+}).catch(console.error);
+var vg_2 = "waste.vg.json";
+vegaEmbed("#map1", vg_2).then(function(result) {
+}).catch(console.error);
