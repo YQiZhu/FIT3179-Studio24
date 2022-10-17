3892f26f0d9eb87fcb858660e291e0ee86506872
YQiZhu <111552343+YQiZhu@users.noreply.github.com>
2022-10-17 17:25:53 +1100

    Update mycode.js

--- Assignment2/mycode.js
+++ Assignment2/mycode.js
@@ -1,21 +1,21 @@
-var vg_1 = "gdp.vg.json";
+var vg_1 = "https://raw.githubusercontent.com/YQiZhu/FIT3179-Studio24/main/Assignment2/gdp.vg.json";
 vegaEmbed("#bubblechart", vg_1).then(function (result) {
 }).catch(console.error);
-var vg_2 = "waste.vg.json";
+var vg_2 = "https://raw.githubusercontent.com/YQiZhu/FIT3179-Studio24/main/Assignment2/waste.vg.json";
 vegaEmbed("#map1", vg_2).then(function (result) {
 }).catch(console.error);
-var vg_3 = "waste_generated_treated.vg.json";
+var vg_3 = "https://raw.githubusercontent.com/YQiZhu/FIT3179-Studio24/main/Assignment2/waste_generated_treated.vg.json";
 vegaEmbed("#bar1", vg_3).then(function (result) {
 }).catch(console.error);
-var vg_4 = "plastic_waste.vg.json";
+var vg_4 = "https://raw.githubusercontent.com/YQiZhu/FIT3179-Studio24/main/Assignment2/plastic_waste.vg.json";
 vegaEmbed("#map2", vg_4).then(function (result) {
 }).catch(console.error);
-var vg_5 = "plastic_disposal.vg.json";
+var vg_5 = "https://raw.githubusercontent.com/YQiZhu/FIT3179-Studio24/main/Assignment2/plastic_disposal.vg.json";
 vegaEmbed("#bar2", vg_5).then(function (result) {
 }).catch(console.error);
-var vg_6 = "type_waste_ocean.vg.json";
+var vg_6 = "https://raw.githubusercontent.com/YQiZhu/FIT3179-Studio24/main/Assignment2/type_waste_ocean.vg.json";
 vegaEmbed("#radial", vg_6).then(function (result) {
 }).catch(console.error);
-var vg_7 = "decomposition.vg.json";
+var vg_7 = "https://raw.githubusercontent.com/YQiZhu/FIT3179-Studio24/main/Assignment2/decomposition.vg.json";
 vegaEmbed("#isotype", vg_7).then(function (result) {
 }).catch(console.error);
