239518ca1dd640d37212006cec64403b7e6c3181
YQiZhu <111552343+YQiZhu@users.noreply.github.com>
2022-10-17 17:17:33 +1100

    Add files via upload

---
+++ Assignment2/mycode.js
@@ -0,0 +1,21 @@
+var vg_1 = "gdp.vg.json";
+vegaEmbed("#bubblechart", vg_1).then(function (result) {
+}).catch(console.error);
+var vg_2 = "waste.vg.json";
+vegaEmbed("#map1", vg_2).then(function (result) {
+}).catch(console.error);
+var vg_3 = "waste_generated_treated.vg.json";
+vegaEmbed("#bar1", vg_3).then(function (result) {
+}).catch(console.error);
+var vg_4 = "plastic_waste.vg.json";
+vegaEmbed("#map2", vg_4).then(function (result) {
+}).catch(console.error);
+var vg_5 = "plastic_disposal.vg.json";
+vegaEmbed("#bar2", vg_5).then(function (result) {
+}).catch(console.error);
+var vg_6 = "type_waste_ocean.vg.json";
+vegaEmbed("#radial", vg_6).then(function (result) {
+}).catch(console.error);
+var vg_7 = "decomposition.vg.json";
+vegaEmbed("#isotype", vg_7).then(function (result) {
+}).catch(console.error);
